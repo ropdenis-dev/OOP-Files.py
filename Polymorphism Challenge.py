7aa37334c52a81eaf33f1b44db9c3335abb3d51d
ropdenis-dev <deniskiptum488@gmail.com>
2025-09-25 00:17:55 +0300

    Polymorphism Challenge.py

---
+++ Polymorphism Challenge.py
@@ -0,0 +1,17 @@
+class Car:
+    def move(self):
+        print("Driving 🚗")
+
+class Plane:
+    def move(self):
+        print("Flying ✈️")
+
+class Boat:
+    def move(self):
+        print("Sailing 🚤")
+
+# Polymorphism in action
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    v.move()   # Each class defines move() differently
